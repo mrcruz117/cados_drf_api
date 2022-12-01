73e8c65a2fdee4d4507530f4082dc7116ee190f1
mrcruz117 <mrcruz1989@gmail.com>
2022-12-01 14:37:03 -0500

    added urls and a more views

--- base/views.py
+++ base/views.py
@@ -5,3 +5,13 @@ from django.http import JsonResponse
 def endpoints(request):
     data = ['/advocates', '/advocates/:username']
     return JsonResponse(data, safe=False)
+
+
+def advocate_list(request):
+    data = ['Dennis', 'Tadas', 'Max']
+    return JsonResponse(data, safe=False)
+
+
+def advocate_detail(request, username):
+    data = username
+    return JsonResponse(data, safe=False)
