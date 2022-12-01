a6600b333bbc308e3c848619f5fa9850ce30b971
mrcruz117 <mrcruz1989@gmail.com>
2022-12-01 16:33:57 -0500

    applied serializer to advocates view

--- base/views.py
+++ base/views.py
@@ -3,16 +3,23 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Advocate
+from .serializers import AdvocateSerializer
+
 
 @api_view(['GET'])
 def endpoints(request):
     data = ['/advocates', '/advocates/:username']
     return Response(data)
 
+
 @api_view(['GET'])
 def advocate_list(request):
-    data = ['Dennis', 'Tadas', 'Max']
-    return Response(data)
+    # data = ['Dennis', 'Tadas', 'Max']
+    advocates = Advocate.objects.all()
+    serializer = AdvocateSerializer(advocates, many=True)
+    return Response(serializer.data)
+
 
 @api_view(['GET'])
 def advocate_detail(request, username):
