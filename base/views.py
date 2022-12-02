2c096b39bb634509edb91c1e7ee90f52c566c6d4
mrcruz117 <mrcruz1989@gmail.com>
2022-12-02 10:33:22 -0500

    added search function based on queries

--- base/views.py
+++ base/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.db.models import Q
 
 from .models import Advocate
 from .serializers import AdvocateSerializer
@@ -15,8 +16,14 @@ def endpoints(request):
 
 @api_view(['GET'])
 def advocate_list(request):
-    # data = ['Dennis', 'Tadas', 'Max']
-    advocates = Advocate.objects.all()
+    query = request.GET.get('query')
+
+    if query == None:
+        query = ''
+
+    print('QUERY: ', query)
+    advocates = Advocate.objects.filter(
+        Q(username__icontains=query) | Q(bio__icontains=query))
     serializer = AdvocateSerializer(advocates, many=True)
     return Response(serializer.data)
 
