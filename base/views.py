3f054bc25923b4f6a632549b715478cfd4f0d347
mrcruz117 <mrcruz1989@gmail.com>
2022-12-02 14:01:15 -0500

    making POST reqs on advocate endpoint

--- base/views.py
+++ base/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -8,24 +8,43 @@ from .models import Advocate
 from .serializers import AdvocateSerializer
 
 
+# GET /advocates
+# POST /advocates
+
+# GET /advocates/:id
+# PUT /advocates/:id
+# DELETE /advocates/:id
+
+
 @api_view(['GET'])
 def endpoints(request):
     data = ['/advocates', '/advocates/:username']
     return Response(data)
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def advocate_list(request):
-    search = request.GET.get('search')
+    # Handles GET reqs
+    if request.method == 'GET':
+        search = request.GET.get('search')
 
-    if search == None:
-        search = ''
+        if search == None:
+            search = ''
 
-    print('search: ', search)
-    advocates = Advocate.objects.filter(
-        Q(username__icontains=search) | Q(bio__icontains=search))
-    serializer = AdvocateSerializer(advocates, many=True)
-    return Response(serializer.data)
+        print('search: ', search)
+        advocates = Advocate.objects.filter(
+            Q(username__icontains=search) | Q(bio__icontains=search))
+        serializer = AdvocateSerializer(advocates, many=True)
+        return Response(serializer.data)
+    # Handles POST reqs
+    if request.method == 'POST':
+
+        advocate = Advocate.objects.create(
+            username=request.data['username'],
+            bio=request.data['bio']
+        )
+        serializer = AdvocateSerializer(advocate, many=False)
+        return Response(serializer.data)
 
 
 @api_view(['GET'])
